7a94bf56a1567e0bdb16a714105eb9fa61a1914e
MrEsteban1 <estebangonz98@gmail.com>
2025-07-01 14:20:51 -0300

    feat: se sube datasources, dto y env config

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import { envs } from "./config"
+import { Server } from "./presentation/server"
+
+(()=>{
+    main()
+})()
+
+async function main(){
+    const server = new Server({port: envs.PORT})
+
+    server.start()
+}
